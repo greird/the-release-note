123badddad05e391dcb66956904792b81dcba4ab
Hubert Fauconnier <h.fauconnier@gmail.com>
2018-10-18 20:45:26 +0200

    moved config in __init__.py; need to fix debug mode

--- ensnare.py
+++ ensnare.py
@@ -1,26 +1,11 @@
 import os, json, logging
 from modules import *
-from pathlib import Path
-
-CONFIG = {
-	'log_path' 			: str(Path.home()) + '/logs/user/',
-	'contact_list_id'	: 5497851, # sendgrid contact list id
-	'released_since' 	: 1, # in days
-	'from_mail' 		: "",
-	'from_name' 		: "The Release Note",
-	'debug' 			: False, # if True, will send newsletter to test_user only
-	'test_user' : [{
-		'email'			: "",
-		'deezer_user_id': 158316,
-		'username'		: "Test User"
-		}]	
-}
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 # create a file handler
-handler = logging.FileHandler(CONFIG['log_path'] + 'ensnare_info.log')
+handler = logging.FileHandler(CONFIG['log_path'] + 'info_the_release_note.log')
 handler.setLevel(logging.INFO)
 handler.setFormatter(formatter)
 # add the handlers to the logger
@@ -32,7 +17,7 @@ dzr = Deezer(
 	app_secret=os.environ.get('APP_SECRET')
 	)
 
-users = getContacts(CONFIG['contact_list_id']) if CONFIG['debug'] == False else CONFIG['test_user']
+users = getContacts(CONFIG['contact_list_id']) if DEBUG['active'] == False else DEBUG['user']
 logger.info(str(len(users)) + ' users found.')
 
 for user in users:
--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,37 @@
+import sys, getopt
 from .deezer import Deezer
 from .template import *
 from .mail import *
+#from pathlib import Path
+
+CONFIG = {
+	'log_path' 			: 'logs/', #str(Path.home()) + '/logs/user/',
+	'contact_list_id'	: 5497851, # sendgrid contact list id
+	'released_since' 	: 1, # in days
+	'from_mail' 		: "ensnare@greird.webfactional.com",
+	'from_name' 		: "The Release Note",
+	}
+
+DEBUG = {
+	'active': False,
+	'user': {
+		'deezer_user_id': 0,
+		'username': '',
+		'email': '',
+	}
+}
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "d", ["debug"])
+except getopt.GetoptError:
+	print("Available option is -d or --debug to run debug mode.")
+	sys.exit(2)
+for opt, arg in opts:
+	if opt in ("-d", "--debug"):
+		print("Configuring debug mode...") 
+		DEBUG['user']['deezer_user_id'] = int(input("Get new releases for user ID: "))
+		DEBUG['user']['username'] = input("Username: ") 
+		DEBUG['user']['email'] = input("Send the newsletter to: ")
+		DEBUG['active'] = True
+		print("Now running in debug mode...") 
+		print("Sending new releases from " + DEBUG['user']['username'] + " (" + str(DEBUG['user']['deezer_user_id']) + ") to " + DEBUG['user']['email'] + "...") 
