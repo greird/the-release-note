ce16b8d206a66aa08f56c08c35adcff89bc5101d
Hubert Fauconnier <h.fauconnier@gmail.com>
2018-10-19 09:27:51 +0000

    renamed

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,13 +2,12 @@ import sys, getopt
 from .deezer import Deezer
 from .template import *
 from .mail import *
-#from pathlib import Path
 
 CONFIG = {
-	'log_path' 			: 'logs/', #str(Path.home()) + '/logs/user/',
+	'log_path' 			: 'logs/',
 	'contact_list_id'	: 5497851, # sendgrid contact list id
 	'released_since' 	: 1, # in days
-	'from_mail' 		: "ensnare@greird.webfactional.com",
+	'from_mail' 		: "the-release-note@greird.webfactional.com",
 	'from_name' 		: "The Release Note",
 	}
 
