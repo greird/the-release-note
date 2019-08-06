ef97ce57fba1fdd87c5e05906a22466c3817ab2b
Hubert Fauconnier <h.fauconnier@gmail.com>
2019-08-06 14:18:43 +0200

    updated options description for clarity

--- modules/options.py
+++ modules/options.py
@@ -12,12 +12,13 @@ parser.add_argument('-u', '--user',
 	metavar=('DEEZER_ID', 'EMAIL'),
 	dest='user',
 	action='append',
-	help='A Deezer user id and an email address.')
+	help='Send all new releases from DEEZER_ID to EMAIL.')
 
 parser.add_argument('-c', '--contact-list', 
 	type=str,
+	metavar='SENDGRID_CONTACT_LIST_ID',
 	dest='contact_list_id',
-	help='A Sendgrid contact list ID.')
+	help='Send a personalized new releases digest to all recipients of a Sendgrid contact list.')
 
 parser.add_argument('-s', '--since', 
 	metavar='NUMBER_OF_DAYS',
